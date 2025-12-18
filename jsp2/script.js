b2fd940640092d3204218744cce9ca106b675ec8
Manuhc8080 <164016601+Manuhc8080@users.noreply.github.com>
2025-12-18 22:16:40 +0530

    commited

---
+++ jsp2/script.js
@@ -0,0 +1,72 @@
+console.log("hey this is toturial 2 of jsp")
+
+var a=5;
+// a = a + 1;
+let b=6;
+
+// use let instead of var
+// var 552 = "manu"; not allow
+// ded
+
+
+
+let c="manohar";
+
+console.log(a+b+9+c)
+// this above experssion is caller arithemetic expresion in jps
+console.log(typeof a, typeof b, typeof c)
+// this is use to find what is the type of variable its   it a number or a charater like this
+
+// const a1 =6;
+
+// a1= a1+6;
+// not allowed because a1 is constlet
+console.log(a)
+{
+
+    let a=66;
+    console.log(a)
+}
+
+var a=66;
+console.log(a)
+
+// var act as a global varibale thats why we use let 
+
+
+
+
+let x="harry bhai";
+let y=22;
+let z= 3.34;
+const p =true;
+let q = undefined;
+let r = null;
+console.log(x. y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z,typeof  p, typeof q,typeof r)
+
+// why type of null is object veryyy imp question in interview
+
+let o = {
+    name: "manu",
+    "job role": 54567,
+    is_handsome: true
+}
+// name is key
+// manu is value
+
+
+console.log(o);
+// this is how we creat a object in javascript
+
+
+
+o.salary = "100 crores"
+console.log(o);
+
+o.salary = "500 crores"
+console.log(o);
+
+
+
+
