c5e0629b1252463865a10f65ba91bcd3f3695a4a
Manuhc8080 <164016601+Manuhc8080@users.noreply.github.com>
2025-12-20 22:25:36 +0530

    commit

---
+++ jsp3/index.js
@@ -0,0 +1,44 @@
+console.log("hello im a box")
+// let age=18;
+// let grace=2;
+
+// age += grace
+
+// === us to evaluate the value and the type
+// open termnila and tyoe node and now u can use thia as a calculator
+// console.log(age)
+// console.log(age)
+// console.log(age*grace)
+// console.log(age/grace)
+// console.log(age**grace)
+// console.log(age%grace)
+// // console.log(age++grace) increment
+// console.log(age--grace) decrement
+
+if (age!=18){
+    console.log("you can drive")
+}
+else if (age == 18){
+   console.log("are you kidding") 
+}
+else{
+    console.log("you cannot drive")
+}
+
+/* im a multiliner 
+comment
+in javascript*/
+
+
+a = 5
+b = 4
+let c = a > b ? (a-b): (b-a);
+// translate to this
+
+// if (a>b){
+//     let c = a-b;
+
+// }
+//  else{
+//     let c= a- b;
+//  }
