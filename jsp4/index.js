f93e00c55e96b0b70756b26406c7b09a72b3e0f9
Manuhc8080 <164016601+Manuhc8080@users.noreply.github.com>
2025-12-21 00:59:50 +0530

    commit

---
+++ jsp4/index.js
@@ -0,0 +1,47 @@
+console.log("thi is the loop statment")
+let a =1;
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+for (let i = 0; i < 100; i++) {
+  console.log(a+i);
+    
+}
+
+let obj = {
+    name: "manu",
+    company : "yoti",
+    Role : "programmer"
+
+
+    // forloop
+
+}
+
+for (const key in obj) {
+
+    
+ 
+    console.log(key )
+    
+    
+}
+for (const  c of "manu") {
+    console.log(c)
+    
+
+    // forof
+}
+
+// let b = 0;
+// while(b>7){
+//     console.log(b)
+// b++;
+// }
+
+let b = 9;
+do {
+    console.log(b)
+     b++;
+
+} while (b<6);
