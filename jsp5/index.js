edc815e8aa49f8fc12c887b8a73592b7dad252c1
Manuhc8080 <164016601+Manuhc8080@users.noreply.github.com>
2025-12-21 09:14:18 +0530

    commit

---
+++ jsp5/index.js
@@ -0,0 +1,37 @@
+
+function habibi(name) {
+
+    console.log("hello " + name + "you are nice")
+    console.log("hey " + name + "you tshirt isnice")
+    console.log("hey " + name + "you are good")
+}
+habibi("rohan")
+habibi("manu")
+
+
+
+console.log("hy manu ur good")
+console.log("hy manu ur tshirt is good")
+console.log("hy manu ur  learn good")
+console.log("hy manu ur nice")
+
+
+
+function sum(a, b, c = 3) {
+    // console.log(a+b)
+    return a + b + c
+}
+result1 = sum(3, 6)
+result2 = sum(37, 6)
+result3 = sum(3, 69,1)
+
+console.log("the sum of a and b is :", result1)
+console.log("the sum of a and b is :", result2)
+console.log("the sum of a and b is :", result3)
+
+
+const func1 =(x) =>{
+
+    console.log("im a man",x)
+}
+func1(43);
---
+++ jsp7/index.js
@@ -0,0 +1,26 @@
+console.log("this is string")
+let a= "harry";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+// console.log(a[5]);
+
+console.log(a.length)
+
+let name= "haarry"
+let friend = "manu"
+console.log("his name is" + name + "and his friend name is" + friend)
+console.log(`his name is ${name} and his friend name is ${friend}`)
+
+
+let b= "manu"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(0,3))
+
+console.log(b.replace("nu","55"))
+console.log(b.concat (a,"55","vee"))
+console.log(b)
